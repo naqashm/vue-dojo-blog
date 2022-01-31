29e08e85c02017589cdee4f4b805a93311873c51
Naqash Malik <49346149+naqashm@users.noreply.github.com>
2022-01-31 18:18:17 +0500

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    
+    <button @click="posts.pop()">delete a post</button>
+  </div>
+</template>
+
+<script>
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+
+export default {
+  name: 'Home',
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts()
+
+    load()
+
+    return { posts, error }
+  }
+
+}
+</script>
